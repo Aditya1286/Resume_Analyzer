bb3070d1aa99491094f0164dc267896e13d3d512
Aditya Aishwarya <aishwaryaaditya2@gmail.com>
2025-07-20 20:36:49 +0530

    added stage2!

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "../../constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "AuthentiCV" },
+    { name: "description", content: "Smart feedback for your CV!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/lg1.jpg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1 className="text-gradient">Track Your Career Moves & CV Performance</h1>
+          <h2 className="text-white">Review Your Career Applications & Get Real-Time AI-Powered Feedback</h2>
+        </div>
+      </section>
+
+
+        {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
